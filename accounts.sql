faff1198f34f06b440d8458304236b3114e5d139
Gabryel <gabryel.ferreira@hotmail.com>
2018-05-09 12:26:30 -0300

    database

---
+++ accounts.sql
@@ -0,0 +1,13 @@
+create database accounts;
+
+use accounts;
+
+create table tb_customer_account(
+id_customer int not null,
+cpf_cnpj varchar(14),
+nm_customer varchar(50),
+is_active boolean,
+vl_total decimal(8, 2),
+primary key(id_customer));
+
+truncate table tb_customer_account;
